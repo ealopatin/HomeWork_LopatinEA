410758f8b6213f343bc33bf2c0558edc30093fae
Lopatin E.A <i@lopatin-ea.ru>
2023-06-29 17:48:58 +0700

    add seminar 2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+
+// Console.Write ("Введите число 1:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write ("Введите число 2:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+// int min = 0;
+
+// if (num1>num2)
+// {
+//     max = num1;
+//     min = num2;
+// }
+// else
+// {
+//     max = num2;
+//     min = num1;
+// }
+
+
+// Console.WriteLine (max);
+
+// Console.WriteLine (min);
+
+// Console.WriteLine ($"Максимальное число: {max}");
+
+// Console.WriteLine ($"Минимальное число: {min}");
+
+// Задача 4: Найти максимальное из трёх чисел.
+
+// Console.Write ("Введите число 1:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write ("Введите число 2:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write ("Введите число 3:");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+
+// if (num1>num2)
+// {
+//     max = num1;
+// }
+// else
+// {
+//     max = num2;
+// }
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+// Console.Write(max);
+
+// Задача 6: Выяснить является ли число чётным.
+
+
+
+// Задача 8: Показать чётные числа от 1 до N.
