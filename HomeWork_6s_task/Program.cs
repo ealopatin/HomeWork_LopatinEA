842eb3ea336d35abeb40298a3eb169baab0d43bb
Lopatin E.A <i@lopatin-ea.ru>
2023-07-14 00:40:44 +0700

    add seminar 6 + HomeWork 6

---
+++ HomeWork_6s_task/Program.cs
@@ -0,0 +1,139 @@
+﻿
+
+//  Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//  сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+// int MessageToValue(string message)
+// {
+
+//     Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+
+// int[] InputArray(int length)
+// {
+//     int[] array = new int[length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = MessageToValue($"Введите элемент {i}: ");
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     Console.WriteLine();
+//     Console.WriteLine($"Массив: [{string.Join("; ", array)}]");
+//     Console.WriteLine();
+// }
+
+// int CountPositiveNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// int length = MessageToValue("Введите размер массива: ");
+// int[] array = InputArray(length);
+
+// PrintArray(array);
+
+// Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(array)}");
+// Console.WriteLine();
+
+
+
+// ПРОВЕРКА НА КОЛИЧЕСТВО ЭЛЕМЕНТОВ МАССИВА БЕЗ ОСТАНОВКИ ПРОГРАММЫ - НЕ РАБОТАЕТ
+
+// int length = MessageToValue("Введите размер массива: ");
+
+// while (true)
+// {
+//     if (length > 10 && length < 0)
+//         Console.Write("Введите значение от 0 до 10 ");
+
+// }
+// int[] array = InputArray(length);
+// PrintArray(array);
+// Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(array)}");
+// Console.WriteLine();
+
+
+// int GetPositivUserNumber(string message, string errorMessage)
+// {
+//     while (true)
+//     {
+//         Console.Write(message);
+//         if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber >= 0)
+//             return userNumber;
+//         Console.WriteLine(errorMessage);
+//     }
+// }
+
+
+
+
+
+// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// //  значения b1, k1, b2 и k2 задаются пользователем.
+
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+// точка пересечения с x у
+
+// k1 * x + b1 = k2 * x + b2
+
+// х = (b2 - b1)/(k1 - k2)
+
+// у = (k1 * (b2 - b1)/(k1 - k2)) + b1
+
+
+// double MessageToValue(string message)
+// {
+
+//     Console.Write(message);
+//     string value = Console.ReadLine();
+//     double result = Convert.ToDouble(value);
+//     return result;
+// }
+
+// double b1 = MessageToValue("Введите значение b1: ");
+// double k1 = MessageToValue("Введите значение k1: ");
+// double b2 = MessageToValue("Введите значение b2: ");
+// double k2 = MessageToValue("Введите значение k2: ");
+
+// double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+// double y = Math.Round((k1 * (b2 - b1) / (k1 - k2)) + b1, 2);
+
+// if (k1 == k2)
+// {
+//     if (b1 == b2)
+//     {
+//         Console.WriteLine();
+//         Console.WriteLine("Прямые совпадают");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Прямые параллельны");
+//     }
+// }
+
+// Console.WriteLine();
+// Console.WriteLine($"Точка пересечения имеет координаты: ({x};{y})");
+// Console.WriteLine();
+
---
+++ Seminar6/Program.cs
@@ -0,0 +1,164 @@
+﻿// Задача 39: Напишите программу, которая перевернёт 
+// одномерный массив (последний элемент будет на первом месте,
+//  а первый - на последнем и т.д.)
+
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+// int[] NewArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+// //     {
+// //         array[i] = new Random().Next(min, max+1);
+// //     }
+// //     return array;
+// // }
+
+// // int[] array = NewArray(5, 1, 10);
+
+// // Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+
+// // // PrintArray(ReverseArray(array));
+
+// // int[] ReverseArray(int[] array)
+
+// // {
+
+// //     int[] reverseArray = new int[array.Length];
+// //     int size = array.Length;
+// //     int j = size-1;
+// //     int temp = 0;
+
+// //     for (int i = 0; i < j; i++, j--)
+// //     {
+// //         temp = array[i];
+// //         array[i] = array[j];
+// //         array[j] = temp;
+// //     }
+// //     return reverseArray;
+// // }
+
+
+// // Console.WriteLine($"Массив: [{String.Join("; ", ReverseArray(array))}]");
+
+
+
+// // //решение преподавателя
+
+
+// int[] NewArray(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+
+// void PrintArray(int[] arr)
+// {
+//     // foreach (int item in arr) //автоматический обход всех символов без условий
+//     // {
+//     //     Console.Write(item + ".."); 
+//     // }
+
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write(arr[i] + "\t");
+
+//     }
+//     Console.Write("]");
+// }
+
+// int[] result = NewArray(9, 0, 15);
+
+// PrintArray(result);
+
+// // Console.WriteLine();
+
+// // PrintArray(CopyArray(result));
+
+// // // //1. Запись в новый массив
+
+// int[] CopyArray (int[] array)
+// {
+//     int[] copyArray = new int [array.Length];
+//     int size = array.Length;
+
+//     for (int start = 0, end = size-1; start < size; start++,end--)
+//     {
+//         copyArray[start] = array[end];    
+//     }
+//     return copyArray;
+// }
+
+// void ReverseArray (int [] array)
+// {
+//     int size= array.Length;
+//     for (int i = 0, j = size - 1; j < size/2; i++, j--)
+//     {
+//             int temp = array [i];
+//             array[i] = array[j];
+//             array[j] = temp;
+        
+//     }
+// }
+
+// Console.WriteLine();
+// ReverseArray(result);
+// PrintArray(result);
+
+
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+// еорема о неравенстве треугольника: 
+// каждая сторона треугольника меньше суммы двух других сторон.
+
+
+// int a = 9;
+// int b = 4;
+// int c = 5;
+
+// bool IsTriangle(int a, int b, int c)
+// {
+//     return (((a+b)> c) && ((b+c)> a) && ((b+c)> a));
+// }
+// if (IsTriangle(a,b,c))
+// {
+//     Console.WriteLine("Треугольник существует");
+// }
+// else
+// {
+//     Console.WriteLine("Треугольник НЕ существует");
+// }
+
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 2)
+// {
+//     return;
+// }
+// int[] fibonacci = new int[N];
+// fibonacci[0] = 0;
+// fibonacci[1] = 1;
+
+// for (int i = 2; i < N; i++)
+// {
+//     fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+// }
+
+// Console.WriteLine($"Массив: {string.Join("; ", fibonacci)}");
+
