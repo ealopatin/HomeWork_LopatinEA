db623674cc83246bcab129962d8c0948aa83f369
Lopatin E.A <i@lopatin-ea.ru>
2023-07-17 14:01:12 +0700

    add seminar 7 + HomeWork 7 task 1,2 и 2

--- HomeWork_7s_task/Program.cs
+++ HomeWork_7s_task/Program.cs
@@ -51,17 +51,93 @@
 // 5 9 2 3
 // 8 4 2 4
 
+
+// Вариант 1. 
+// Проверка без метода
+
+
+// Console.Write("Введите колчиество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите колчиество столбцов: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+
+
+// int minRandom = 0;
+// int maxRandom = 10;
+// int[,] array2D = GetMatrix(rows, columns, minRandom, maxRandom);
+// PrintMatrix(array2D);
+// Console.WriteLine();
+
+
+// int[,] GetMatrix(int m, int n, int min, int max)
+// {
+//     int[,] matrix = new int[m, n]; //m строки 
+//     for (int i = 0; i < matrix.GetLength(0); i++) //m цикл по строкам //GetMAtrix.GetLength(0) сколько строк //GetMAtrix.GetLength(0) сколько сстольбцов
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++) //n цикл по столбцам 
+//         {
+//             matrix[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write(matrix[i, j] + "\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Console.Write("Строку: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+
+// Console.Write("Столбец: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+
+// if (m > rows || n > columns)
+// {
+//     Console.Write("Такого числа в массиве нет");
+// }
+// else
+// {
+// Console.WriteLine($"Число по указанной позиции: {array2D[m, n]}");
+// }
+
+
+// 2 Вариант 
+// Проверка и запрос повторного ввода без завершения программы
+
 Console.Write("Введите колчиество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите колчиество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
 int minRandom = 0;
 int maxRandom = 10;
+int rowsIndex = 0;
+int columnsIndex = 0;
+
 int[,] array2D = GetMatrix(rows, columns, minRandom, maxRandom);
+
 PrintMatrix(array2D);
 Console.WriteLine();
+
+CheckIndexElement(rowsIndex,columnsIndex);
+Console.WriteLine($"Число по указанной позиции: {array2D[rowsIndex, columnsIndex]}");
 
 
 int[,] GetMatrix(int m, int n, int min, int max)
@@ -90,57 +166,32 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-
-Console.Write("Строку: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-
-Console.Write("Столбец: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-
-if (m > rows || n > columns)
+void CheckIndexElement(int crowsIndexCheck, int columnsIndexCheck)
 {
-    Console.Write("Такого числа в массиве нет");
+    while (true)
+    {
+        Console.Write("Введите индекс строки: ");
+        rowsIndex = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите индекс столбца: ");
+        columnsIndex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+        if ((rowsIndex >= 0 && rowsIndex <= rows-1) || (columnsIndex >= 0 && columnsIndex <= columns-1))
+        {
+            break;
+        }
+        Console.WriteLine("Такого числа в массиве нет.");
+        Console.WriteLine();
+        Console.Write($"Введите корректные значения позиции элемента - не более {rows-1} для строк и не более {columns-1} для столбцов.");
+        Console.WriteLine();
+
+    }
+
 }
-else
-{
-Console.WriteLine($"Число по указанной позиции: {array2D[m, n]}");
-}
 
 
-// Проверка и запрос повторного ввода без запвершения программы
-// void CheckElement(int ckeckElement)
-// {
-//     while (true)
-//     {
-//         Console.Write("Строку: ");
-//         int m = Convert.ToInt32(Console.ReadLine());
 
-//         Console.Write("Столбец: ");
-//         int n = Convert.ToInt32(Console.ReadLine());
-
-//         if (m < rows || n < columns)
-//         {
-//             break;
-//         }
-//         Console.Write("Введите корректное значение: ");
-
-//     }
-
-// }
-
-// CheckElement(array2D);
-
-// Console.WriteLine(array2D[m, n]);
-
-// while
-// if (m > rows || n > columns)
-//     {
-//         Console.WriteLine("Такого значения нет");
-//         break;
-//     }
-// Console.WriteLine(array2D[m, n]);
 
 
 // 17 -> такого числа в массиве нет
