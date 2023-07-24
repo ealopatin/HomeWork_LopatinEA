8b3f62994ef16b7708b104eef167e5bc7ba10a00
Lopatin E.A <i@lopatin-ea.ru>
2023-07-25 02:31:08 +0700

    add seminanr 9 + homework 9 last task

---
+++ HomeWork_9s_task/Program.cs
@@ -0,0 +1,97 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+
+
+// Console.WriteLine("Введите число:");
+// int numM = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число:");
+// int numN = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNum(int numM, int numN)
+// {
+//     if (numM == numN) return numM.ToString();
+
+//     return numM + PrintNum(numM + 1, numN);
+// }
+
+// Console.WriteLine($"Числа от {numM} до {numN}: {PrintNum(numM, numN)}");
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15-> 120
+// M = 4; N = 8. -> 30
+
+// Вариант 1
+
+// Console.WriteLine("Введите число:");
+// int numM = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число:");
+// int numN = Convert.ToInt32(Console.ReadLine());
+
+
+// string PrintNum(int numM, int numN)
+// {
+//     if (numM == numN) return numM.ToString();
+
+//     return numM + PrintNum(numM + 1, numN);
+// }
+
+// Console.WriteLine($"Числа от {numM} до {numN}: {PrintNum(numM, numN)}");
+
+// int arrayMN = Convert.ToInt32(PrintNum(numM, numN));
+
+// int SumNum(int num)
+// {
+//     if (num == 0) return 0;
+//     return num % 10 + SumNum(num / 10);
+// }
+// Console.WriteLine($"Cумма цифр в числе {arrayMN}: {SumNum(arrayMN)}");
+
+// Вариант 2
+
+// Console.WriteLine("Введите число:");
+// int numM = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число:");
+// int numN = Convert.ToInt32(Console.ReadLine());
+
+
+// int PrintNum(int numM, int numN)
+// {
+//     if (numM == numN) return numM;
+
+//     return numM + PrintNum(numM + 1, numN);
+// }
+
+// Console.WriteLine($"Сумма чисел от {numM} до {numN}: {PrintNum(numM, numN)}");
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n. 
+// m = 2, n = 3->A(m, n) = 29
+
+
+// Console.WriteLine("Введите число:");
+// int numM = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число:");
+// int numN = Convert.ToInt32(Console.ReadLine());
+
+
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     else if (n == 0) return Akkerman(m - 1, 1);
+//     return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+
+// Console.WriteLine(Akkerman(numN, numM));
---
+++ Seminar9/Program.cs
@@ -0,0 +1,168 @@
+﻿// // Рекурсия - вызов функции внутри функции
+
+// // 3! = 1 * 2 * 3 = 6 (N! - "Факториал от числа N")
+// // Факториал - произв-е чисел от 1 до N включительно 
+
+// int Factorial(int n)
+// {
+//     // Базовый случай - случай, когда нужно выйти из рекурсии 
+//     if (n == 1) return 1; // Попадание в return = завершение функции
+//     // Рекурсивный случай
+//     return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(4));
+
+// Задача 63: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6" (1 -> N)
+
+// N! = 24 (1 -> N или N <- 1)
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int start, int end) // start = 1, end = n
+// {
+//     // Базовый случай
+//     if (start == end) return start.ToString(); // Перевод в строчку
+//     // Рекурсивный случай - вызвать функцию внутри функции 
+//     return start + ", " + PrintNumbers(start + 1, end);
+// }
+
+// Console.WriteLine($"Числа от 1 до {n}: {PrintNumbers(1, n)}");
+
+// Задача 67: Напишите программу, которая будет принимать
+//  на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Console.Write("Введите число N: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumbers(int num)
+// {
+//     // Базовый случай
+//     if (num == 0) return 0;
+//     // Рекурсивный
+//     return num % 10 + SumNumbers(num / 10);
+
+// }
+// Console.WriteLine($"Cумма цифр в числе {number}: {SumNumbers(number)}");
+
+
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// Console.Write("Введите число A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите степень В: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// int Pow(int num, int rank)
+// {
+//     // Базовые случаИ (их тут 2)
+//     if (rank == 0) return 1;
+//     if (rank == 1) return num;
+//     // Рекурсивный случай
+//     return num * Pow(num, rank - 1);
+// }
+// if(B < 0)
+// {
+//     System.Console.WriteLine("Степень меньше 0 мы не обрабатываем");
+//     return;
+// }
+// Console.WriteLine($"{A} в степени {B} = {Pow(A, B)}");
+
+
+// / Семинар
+
+// // Рекурсия - вызов функции внутри функции
+
+// // 3! = 1 * 2 * 3 = 6 (N! - "Факториал от числа N")
+// // Факториал - произв-е чисел от 1 до N включительно 
+
+// int Factorial(int n)
+// {
+//     // Базовый случай - случай, когда нужно выйти из рекурсии 
+//     if (n == 1) return 1; // Попадание в return = завершение функции
+//     // Рекурсивный случай
+//     return n * Factorial(n - 1);
+// }
+// Console.WriteLine(Factorial(4));
+// Console.WriteLine(Factorial(5));
+
+// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// string natural (int number)
+// {
+//     if (number == 1) return 1;
+
+//     return Console.Write(number + natural(number)+" ");
+//     number--;
+// }
+
+// natural(number);
+
+
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// string natural (int start, int end)
+// {
+//     if (start == end) return start.ToString();
+
+//     return start + ", " + natural(start+1, end);
+//     start++;
+// }
+
+// Console.Write(natural(1, number));
+
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumver(int number)
+// {
+//     if (number == 0) return 0;
+//     return number % 10 + SumNumver(number / 10);
+// }
+// Console.Write(SumNumver(number));
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// Console.WriteLine("Введите число:");
+// int numA = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число:");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// int Numpower(int a, int b)
+// {
+//     if (b == 0) return 1;
+//     return a * Numpower(a, b - 1);
+// }
+// Console.Write(Numpower(numA,numB));
+
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 6, 7, 8"
+
